7026e22ff08725a8827bceef90b11ae5b90e9d99
sambhav-007 <sambhav.sehgal.007@gmail.com>
2025-10-25 20:19:35 +0530

    Feat: Increase opacity of dark overlay on HomePage video background

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -43,7 +43,7 @@ function HomePage() {
         </video>
 
         {/* Dark Overlay */}
-        <div className="absolute inset-0 bg-black opacity-50 z-10"></div>
+        <div className="absolute inset-0 bg-black opacity-65 z-10"></div>
 
         {/* Content Overlay */}
         <div className="relative z-20 text-center px-4"> {/* Increased z-index */}
